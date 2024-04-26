ba162d0adc2adcad4e4645f0eab14db1fdf559a0
Batroman <o.lonnvald@gmail.com>
2024-04-26 10:04:05 +0200

    h

--- resources/iMatApp/src/imat/iMatApp.java
+++ resources/iMatApp/src/imat/iMatApp.java
@@ -35,8 +35,7 @@ public class iMatApp extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) {launch(args);
     }
     
 }
