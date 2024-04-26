99549f8441c5980d3c143bb5eebf21d835ef6b3d
Batroman <o.lonnvald@gmail.com>
2024-04-26 16:28:55 +0200

    h

---
+++ resources/iMatApp/src/imat/iMatProduct.java
@@ -0,0 +1,23 @@
+package imat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class IMatProduct {
+    @FXML private AnchorPane productView;
+    @FXML private ImageView productImage;
+    @FXML private ImageView favoriteLogo;
+    @FXML private Label productNameLabel;
+    @FXML private Label productPriceLabel;
+    @FXML private Button buyProductButton;
+    @FXML private Pane infoPane;
+
+    public Product(Product product){
+
+    }
+
+}
